6c417ce04dfc57d3c77037a73057f1ef25d8677d
Navarro <antonio.navarro@iesnervion.es>
2023-05-02 12:24:35 +0200

    Problemas con el Jar2

--- src/ejercicios/ejercicio1/Main.java
+++ src/ejercicios/ejercicio1/Main.java
@@ -1,7 +1,9 @@
 package ejercicios.ejercicio1;
 
+import funciones.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Mathematics.rng();
     }
 }
